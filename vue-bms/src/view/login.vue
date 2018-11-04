23d08e73d83e3125d1aecb9396a7100521693bc1
nhclike <nhc201108>
2018-11-04 20:25:55 +0800

    初始化程序

---
+++ vue-bms/src/view/login.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        {{msg}}
+    </div>
+</template>
+
+
+<script>
+    export default{
+        data () {
+            return {msg: '这个是Home模板页'}
+        }
+    }
+</script>
+
+<style>
+    body {
+        background-color: #ff0000;
+    }
+</style>
